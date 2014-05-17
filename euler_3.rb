1714adc780bdf157d925bb702e9f098cbdc2d667
cheeseandpepper <cheeseandpepper@gmail.com>
2014-05-16 22:54:47 -0400

    added #3

---
+++ euler_3.rb
@@ -0,0 +1,41 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+
+def is_prime?(num)
+  n = 2
+  value = false
+  while n < num
+    if num % n == 0
+      value = false
+      return value
+    else
+      value = true
+      n += 1
+    end
+  end
+  value
+end
+
+
+def largest_prime_factor(number)
+
+  i = Math.sqrt(number).truncate
+  found = false
+
+  while found == false && i > 0
+    if number % i == 0 && is_prime?(i)
+      puts i
+      return i
+    else
+      i -= 1
+      
+    end
+
+  end
+
+end
+
+
+largest_prime_factor(600851475143)
