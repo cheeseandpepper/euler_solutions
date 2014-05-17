e21a95711c906b7364cd58f4af064ac5ab6cfe9c
cheeseandpepper <cheeseandpepper@gmail.com>
2014-05-16 23:34:11 -0400

    added 4

---
+++ euler_4.rb
@@ -0,0 +1,25 @@
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def largest_palindrome_from_product
+
+  max = 9999
+  min = 1000
+  product = 0
+
+  max.downto(min).each do |a|
+    max.downto(min).each do |b|
+      product = a*b if is_palindrome?(a*b) && a*b > product
+    end
+  end
+
+puts product
+
+end
+
+def is_palindrome?(num)
+  num.to_s == num.to_s.reverse && num.to_s != 0
+end
+
+largest_palindrome_from_product
